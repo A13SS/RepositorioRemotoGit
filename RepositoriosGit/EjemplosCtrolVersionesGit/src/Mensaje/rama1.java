0562938d8d0d277ab48d19b534debf8fc1a5d5d1
A13SS <aferesc1409@g.educaand.es>
2025-03-01 16:40:30 +0100

    actualizada rama1

--- RepositoriosGit/EjemplosCtrolVersionesGit/src/Mensaje/rama1.java
+++ RepositoriosGit/EjemplosCtrolVersionesGit/src/Mensaje/rama1.java
@@ -4,6 +4,8 @@ public class rama1 {
 	public static void main(String[] args) {
 		 System.out.println("Mensaje por pantalla1");
 		 System.out.println("Mensaje por pantalla2");
+		 System.out.println("Mensaje por pantalla3");
+		 System.out.println("Mensaje por pantalla4");
 
 	}
 }
