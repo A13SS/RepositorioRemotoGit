d03a515901058a0d841e303a5771daf1131c20f7
A13SS <aferesc1409@g.educaand.es>
2025-03-01 16:38:14 +0100

    añadida rama1

---
+++ RepositoriosGit/EjemplosCtrolVersionesGit/src/Mensaje/rama1.java
@@ -0,0 +1,9 @@
+package Mensaje;
+
+public class rama1 {
+	public static void main(String[] args) {
+		 System.out.println("Mensaje por pantalla1");
+		 System.out.println("Mensaje por pantalla2");
+
+	}
+}
